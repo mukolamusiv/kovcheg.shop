2c0910d9ebb0b11bffe60925a114823f9babf1f8
Mykola Mysiv <mukolamusiv@gmail.com>
2026-03-04 14:06:30 +0200

    Update OrdersTable.php

--- app/Filament/Resources/Orders/Tables/OrdersTable.php
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -29,7 +29,7 @@ class OrdersTable
                             'оформлено' => 'info',
                             'виготовляється' => 'warning',
                             'виготовлено' => 'success',
-                            'завершено' => 'secondary',
+                            'готове' => 'secondary',
                             'скасовано' => 'danger',
                             default => 'info',
                         })
