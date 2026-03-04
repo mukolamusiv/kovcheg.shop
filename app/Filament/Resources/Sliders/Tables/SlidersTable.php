51931bb09993eccfb4c1d02bb79a87ae5b86f1ed
Mykola Mysiv <mukolamusiv@gmail.com>
2026-03-04 13:28:56 +0200

    Update SlidersTable.php

--- app/Filament/Resources/Sliders/Tables/SlidersTable.php
+++ app/Filament/Resources/Sliders/Tables/SlidersTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -20,7 +21,7 @@ class SlidersTable
                 TextColumn::make('subtitle')->sortable()->searchable(),
                 TextColumn::make('image_path')->sortable(),
                 TextColumn::make('link')->sortable(),
-                TextColumn::make('is_active')->boolean()->sortable(),
+                IconColumn::make('is_active')->boolean()->sortable(),
                // TextColumn::make('created_at')->dateTime()->sortable(),
             ])
             ->filters([
