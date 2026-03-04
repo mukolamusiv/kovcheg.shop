bb34b79b62c0d6a71b9ae4e5236863fa35373b84
Mykola Mysiv <mukolamusiv@gmail.com>
2026-03-04 14:52:59 +0200

    Update SupplinerForm.php

--- app/Filament/Resources/Suppliners/Schemas/SupplinerForm.php
+++ app/Filament/Resources/Suppliners/Schemas/SupplinerForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Suppliners\Schemas;
 
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class SupplinerForm
@@ -10,7 +11,38 @@ class SupplinerForm
     {
         return $schema
             ->components([
-                //
+                Section::make('Інформація користувача')
+                    ->schema([
+                        \Filament\Forms\Components\TextInput::make('user_id')
+                            ->label('Користувач')
+                            ->required()
+                            ->numeric(),
+                        \Filament\Forms\Components\TextInput::make('phone')
+                            ->label('Телефон')
+                            ->tel()
+                            ->nullable(),
+                        \Filament\Forms\Components\TextInput::make('address')
+                            ->label('Адреса')
+                            ->nullable(),
+                    ]),
+                Section::make('Особисті дані')
+                    ->schema([
+                        \Filament\Forms\Components\DatePicker::make('date_of_birth')
+                            ->label('Дата народження')
+                            ->nullable(),
+                        \Filament\Forms\Components\TextInput::make('city')
+                            ->label('Місто')
+                            ->nullable(),
+                        \Filament\Forms\Components\Textarea::make('note')
+                            ->label('Примітка')
+                            ->nullable(),
+                    ]),
+                Section::make('Вимірювання')
+                    ->schema([
+                        \Filament\Forms\Components\KeyValue::make('measurements')
+                            ->label('Вимірювання')
+                            ->nullable(),
+                    ]),
             ]);
     }
 }
